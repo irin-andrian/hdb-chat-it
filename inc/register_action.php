b846cf5027c2f8a464346328dc60a0c20ae5bf76
Roger Rakotofiringa <roger.k@hdb-solutions.com>
2018-05-28 17:22:42 +0200

    correct redirect html to php

--- inc/register_action.php
+++ inc/register_action.php
@@ -7,5 +7,5 @@ SET username	= '" . $_POST['username']. "',
 $result = $mysqli->query ($sql);
 $_SESION['username'] = $_POST['username'];
 $_SESION['id'] = $_POST['id'];
-header('Location: http://hdb-chat-it.local/index.php');
+header('Location: http://chat-it.hdb-solutions.local/index.php');
 ?>
