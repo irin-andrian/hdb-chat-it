868e07189d16a3afd6b192fb5dfa881b18242766
Roger Rakotofiringa <roger.k@hdb-solutions.com>
2018-05-28 11:03:25 +0200

    translate the page

--- pages/examples/register.php
+++ pages/examples/register.php
@@ -21,16 +21,16 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>Administration</b>des comptes</a>
   </div>
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">Creer un nouveau compte</p>
 
       <form action="../../index.html" method="post">
         <div class="form-group has-feedback">
-          <input type="text" class="form-control" placeholder="Full name">
+          <input type="text" class="form-control" placeholder="Nom complet">
           <span class="fa fa-user form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
@@ -38,42 +38,31 @@
           <span class="fa fa-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Mot de passe">
           <span class="fa fa-lock form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" class="form-control" placeholder="Retaper le mot de passe">
           <span class="fa fa-lock form-control-feedback"></span>
         </div>
         <div class="row">
           <div class="col-8">
             <div class="checkbox icheck">
               <label>
-                <input type="checkbox"> I agree to the <a href="#">terms</a>
               </label>
             </div>
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+            <button type="submit" class="btn btn-primary btn-block btn-flat">S'inscrire</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
-      <div class="social-auth-links text-center">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fa fa-facebook mr-2"></i>
-          Sign up using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fa fa-google-plus mr-2"></i>
-          Sign up using Google+
-        </a>
-      </div>
+      
 
-      <a href="login.html" class="text-center">I already have a membership</a>
+      
     </div>
     <!-- /.form-box -->
   </div><!-- /.card -->
