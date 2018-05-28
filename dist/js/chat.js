4e2d59161edafa0c37a04e63385df4e71383433f
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-05-28 11:56:29 +0300

    add fuction getStateOfChat

--- dist/js/chat.js
+++ dist/js/chat.js
@@ -8,3 +8,24 @@ function Chat () {
     this.send = sendChat;
 	this.getState = getStateOfChat;
 }
+
+//gets the state of the chat
+function getStateOfChat(){
+	if(!instanse){
+		 instanse = true;
+		 $.ajax({
+			   type: "POST",
+			   url: "process.php",
+			   data: {  
+			   			'function': 'getState',
+						'file': file
+						},
+			   dataType: "json",
+			
+			   success: function(data){
+				   state = data.state;
+				   instanse = false;
+			   },
+			});
+	}	 
+}
