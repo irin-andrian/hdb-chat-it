0fa03b7c144a92d437e3562e2f569f82659b06bf
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-05-28 12:24:02 +0300

    add action getState Chat

--- chatprocess.php
+++ chatprocess.php
@@ -5,6 +5,12 @@
     $log = array();
 
     switch($function) {
+        case('getState'):
+        if(file_exists('chat.txt')){
+          $lines = file('chat.txt');
+        }
+        $log['state'] = count($lines); 
+        break;	
        
    }
    echo json_encode($log);
