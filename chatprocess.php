c18d135acea0853ad7f8d68add66a918ee66ce8e
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-05-28 12:25:06 +0300

    add action update Chat

--- chatprocess.php
+++ chatprocess.php
@@ -12,6 +12,31 @@
         $log['state'] = count($lines); 
         break;	
        
+        case('update'):
+        	$state = $_POST['state'];
+        	if(file_exists('chat.txt')){
+        	   $lines = file('chat.txt');
+        	 }
+        	 $count =  count($lines);
+        	 if($state == $count){
+        		 $log['state'] = $state;
+        		 $log['text'] = false;
+        		 
+        		 }
+        		 else{
+        			 $text= array();
+        			 $log['state'] = $state + count($lines) - $state;
+        			 foreach ($lines as $line_num => $line)
+                       {
+        				   if($line_num >= $state){
+                         $text[] =  $line = str_replace("\n", "", $line);
+        				   }
+         
+                        }
+        			 $log['text'] = $text; 
+        		 }
+        	  
+             break;
    }
    echo json_encode($log);
 
