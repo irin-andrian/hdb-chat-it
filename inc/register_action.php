e15019cb839ece142c5a3550b5506116477504ee
Roger Rakotofiringa <roger.k@hdb-solutions.com>
2018-05-28 15:56:53 +0200

    register action

---
+++ inc/register_action.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include ("db_connect.php");
+$sql = "INSERT INTO users
+SET username	= '" . $_POST['username']. "',
+    pwd	= '" . $_POST['pwd'] . "'";
+
+
+
+
+$result = $mysqli->query ($sql);
+$_SESION['username'] = $_POST['username'];
+$_SESION['pwd'] = $_POST['pwd'];
+header('Location: http://hdb-chat-it.local');
+
+
+?>
