d62ce642cfa64f13fe115a172a22cf84afac29f1
rado <rado.r@hdb-solutions.com>
2018-05-29 15:50:17 +0300

    resolve conflit

--- inc/login_action.php
+++ inc/login_action.php
@@ -4,17 +4,19 @@ include 'db_connect.php';
 $username = $_POST['username'];
 $pwd = sha1($_POST['pwd']);
 
+
 if($username != null && $pwd != null){
     $sql = "SELECT * FROM users  WHERE users.username='".$username."' and users.pwd='".$pwd."'";
     $result = $mysqli->query ($sql);
     $data = $result->fetch_all(MYSQLI_ASSOC);
-    if(empty($data)){
-        header("Location: /login.php?loginfailed");
-        exit;
-    }
-    $_SESION['username'] = $_POST['username'];
-	$_SESION['id'] = $data[0]['id'];
-	header("Location: /global.php?id=".$data[0]['id']);
+     if($data !=null){
+         $_SESSION['username'] = $_POST['username'];
+         $_SESSION['id'] = $data[0]['id'];
+
+         header("Location: /global.php?id=".$data[0]['id']."&username=".$data[0]['username']);
+     }else{
+         header("Location: /login.php");
+     }
 }else{
     header("Location: /login.php");
 }
