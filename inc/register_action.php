f4d65788b9a26ef4e3ed3ecf50f5378af5ffc13a
Silaniaina Sandra <sandra.r@hdb-solutions.com>
2018-05-29 10:23:36 +0300

    resolve register action to global page

--- inc/register_action.php
+++ inc/register_action.php
@@ -2,10 +2,11 @@
     session_start();
     include 'db_connect.php';
     $sql = "INSERT INTO users SET username	= '" . $_POST['username']. "',pwd	= '" . $_POST['pwd'] . "'";
-    $result = $mysqli->query ($sql);
-    if($result) {
-        $id = $result->mysql_insert_id();
+    if($mysqli->query ($sql)) {
+        header("Location: /global.php?id=".$mysqli->insert_id."&username=".$_POST['username']);
+    }else{
+        header("Location: /register.php");
     }
-    header("Location: /global.php?id=".$id."&username=".$_POST['username']);
+    
 ?>
      
